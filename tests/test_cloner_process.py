77b72a868966d2aeb00e6d7b14bf7cb32780eb55
Ramon <ramon.guimera@skyscanner.net>
2022-04-15 15:17:14 +0200

    added tests for cloner_process.py

--- tests/test_cloner_process.py
+++ tests/test_cloner_process.py
@@ -17,3 +17,16 @@ def test_cloner_process_can_clone_a_repo(
     mock_execute_system_command.assert_called_once_with(
         command=f"git clone {random_repository.clone_url} --quiet ./repos/{random_repository.name}"
     )
+
+
+@patch.object(ClonerProcess, "_execute_system_command")
+def test_cloner_process_wont_clone_if_given_list_of_repositories_is_empty(
+    mock_execute_system_command, random_repository
+):
+    mock_execute_system_command.return_value = 0
+
+    cloner_process = ClonerProcess(repos_to_clone=[], process_id=1)
+
+    cloner_process.run()
+
+    mock_execute_system_command.assert_not_called()
