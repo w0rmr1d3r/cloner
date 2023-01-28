b4115cc4e012d7974ec676d8698a1dc2015ed609
Ramon <ramon.guimera@skyscanner.net>
2023-01-28 16:36:09 +0100

    migrate to pyproject.toml

--- cloner/__version__.py
+++ cloner/__version__.py
@@ -1,2 +1,7 @@
-# If this changes in major, update the SECURITY.md file accordingly.
-__version__ = "1.8.0"
+"""
+Exposing version
+Update this file accordingly with pyproject.toml
+"""
+
+__version__ = "1.9.0"
+VERSION = __version__.split(".")
