6f5ccdee802adefc83652235b7b47a92d6b587e7
Ramon <ramon.guimera@skyscanner.net>
2023-01-28 16:45:38 +0100

    improve coverage and pylint for repository

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -3,10 +3,10 @@ from cloner.repository import Repository
 
 def test_repository_as_string(random_repository):
     name = random_repository.name
-    assert random_repository.__str__() == f"Repo: {name} URL: https://github.com/organization/{name}.git"
+    assert str(random_repository) == f"Repo: {name} URL: https://github.com/organization/{name}.git"
 
 
-def test_repository_can_be_compared(faker):
+def test_repository_can_be_compared():
     repository_one = Repository(
         name="repo_one",
         clone_url="https://github.com/organization/repo_one.git",
@@ -18,3 +18,26 @@ def test_repository_can_be_compared(faker):
         repo_id=1,
     )
     assert repository_one == repository_other
+
+
+def test_repository_can_be_compared_and_is_different():
+    repository_one = Repository(
+        name="repo_one",
+        clone_url="https://github.com/organization/repo_one.git",
+        repo_id=2,
+    )
+    repository_other = Repository(
+        name="repo_one",
+        clone_url="https://github.com/organization/repo_one.git",
+        repo_id=1,
+    )
+    assert repository_one != repository_other
+
+
+def test_repository_can_be_compared_and_is_false_when_compared_to_another_object():
+    repository_one = Repository(
+        name="repo_one",
+        clone_url="https://github.com/organization/repo_one.git",
+        repo_id=2,
+    )
+    assert repository_one != int(1)
