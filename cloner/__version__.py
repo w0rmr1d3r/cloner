6ad59fe7e3a359ce06a8f0b1fbc09f04fe8df93f
Ramon <ramon.guimera@skyscanner.net>
2023-02-01 12:04:34 +0100

    fix doc formatting

--- cloner/__version__.py
+++ cloner/__version__.py
@@ -1,6 +1,6 @@
-"""
-Exposing version and used on the cli command
-Update this file accordingly with pyproject.toml
+"""Exposing version and used on the cli command.
+
+Update this file accordingly with pyproject.toml.
 """
 
 __version__ = "1.9.0"
